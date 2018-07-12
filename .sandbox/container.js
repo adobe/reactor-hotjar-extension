4f3d71d3a35a860982c70f737e42b7116a23a657
Celeste Robinson <crobinso@adobe.com>
2018-07-12 15:12:31 -0600

    added platform property to extension.json

--- .sandbox/container.js
+++ .sandbox/container.js
@@ -5,7 +5,7 @@ module.exports = {
     "hotjar": {
       "displayName": "Hotjar",
       "settings": {
-        "siteId": "930763"
+        "siteId": "944579"
       }
     }
   },
@@ -14,8 +14,8 @@ module.exports = {
   },
   "buildInfo": {
     "turbineVersion": "25.1.0",
-    "turbineBuildDate": "2018-07-10T18:40:12.598Z",
-    "buildDate": "2018-07-10T18:40:12.598Z",
+    "turbineBuildDate": "2018-07-11T16:30:07.846Z",
+    "buildDate": "2018-07-11T16:30:07.846Z",
     "environment": "development"
   }
 }
